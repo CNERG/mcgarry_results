5bfcb19c3e1e1de410ce72c1da32621d0ccbedd0
Meghan McGarry <mbmcgarry@wisc.edu>
2015-07-17 16:45:21 -0500

    updates to database, during conversion of random_sink to v1.3

---
+++ cyan_tests/many_sims.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo ' {"facinv": " SELECT tl.Time AS Time,IFNULL(sub.qty, 0) AS Quantity FROM timelist as tl LEFT JOIN ( SELECT tl.Time as time,SUM(inv.Quantity) AS qty FROM inventories as inv JOIN timelist as tl ON UNLIKELY(inv.starttime <= tl.time) AND inv.endtime > tl.time AND tl.simid=inv.simid JOIN agents as a on a.agentid=inv.agentid AND a.simid=inv.simid WHERE a.AgentId=?  GROUP BY tl.Time) AS sub ON sub.time=tl.time "}' > query.json
+
+for i in $(seq 1); do
+    out=run-${i}.sqlite
+    cyclus -o $out $1
+    cyan -db $out post
+
+    for fac_id in 16 17 18; do
+        fname=run-${i}-fac-${fac_id}.dat
+        ./cyanb -db $out -custom query.json facinv $fac_id > $fname
+    done
+done;
+
