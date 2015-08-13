5d597b72eeebe9435ad43f46b76bd310af186279
Meghan McGarry <mbmcgarry@wisc.edu>
2015-08-13 15:51:54 -0700

    cleaning up again and adding pu_reprocessing directory

---
+++ testing/random_sep/multi_sims.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+X=100
+
+for i in $(seq $X); do
+    out=run-${i}.sqlite
+#    CYCLUS_DEBUG_DRE=1 cyclus -o $out $1 -v LEV_DEBUG5 > $i.out
+    CYCLUS_DEBUG_DRE=1 cyclus -o $out $1 > $i.out
+done;
+
+# Compare results
+for i in $(seq $X) ; do
+    diff ${i}.out ${i+1}.out >> diff.out
+done;
+
