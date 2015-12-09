b14983263f4a7c15072beec1a6df49ab497487b6
Meghan McGarry <mbmcgarry@wisc.edu>
2015-12-09 17:00:12 -0600

    test case for UM. Variance appears to be too large

---
+++ UM_data/multi_modal_v1.3/just_process.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo ' {"facinv": " SELECT tl.Time AS Time,IFNULL(sub.qty, 0) AS Quantity FROM timelist as tl LEFT JOIN ( SELECT tl.Time as time,SUM(inv.Quantity) AS qty FROM inventories as inv JOIN timelist as tl ON UNLIKELY(inv.starttime <= tl.time) AND inv.endtime > tl.time AND tl.simid=inv.simid JOIN agents as a on a.agentid=inv.agentid AND a.simid=inv.simid WHERE a.AgentId=?  GROUP BY tl.Time) AS sub ON sub.time=tl.time "}' > query.json
+
+for fac_id in 15 16; do
+    fname=${fac_id}_${1}.dat
+    ~/git/data_analysis/data/cyanb -db $1 -custom query.json facinv $fac_id > $fname
+done;
+
